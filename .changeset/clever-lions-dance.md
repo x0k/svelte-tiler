1ebd363fc9cb07d40bfc63519b7c89d30c44bd28
Roman Krasilnikov <x0kill@ya.ru>
2026-02-13 19:05:41 +0300

    Set `script: true` for Vite preprocessor

---
+++ .changeset/clever-lions-dance.md
@@ -0,0 +1,7 @@
+---
+'svelte-tiler': patch
+---
+
+Set `script: true` for Vite preprocessor.
+
+Issue: <https://github.com/sveltejs/svelte/issues/17657>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({ script: true }),
 
   kit: {
     adapter: adapter(),
