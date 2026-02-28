b10f55f297dea98c5302fc6e101465aab1afb830
Roman Krasilnikov <x0kill@ya.ru>
2026-03-01 00:28:38 +0300

    Bump deps

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess({ script: true }),
-
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
   kit: {
     adapter: adapter(),
   },
